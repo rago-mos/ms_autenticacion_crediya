80e89fac858b950c0c843d0c795bd7bfb42b79f2
rago.mos <ralberto115@gmail.com>
2025-08-26 20:12:17 -0500

    refactor: cambio de bd a mysql, se configuran propiedades

---
+++ applications/app-service/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,25 @@
+-- Tabla de roles
+CREATE TABLE IF NOT EXISTS rol (
+    id_rol INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Tabla de usuarios
+CREATE TABLE IF NOT EXISTS usuario (
+    id_usuario BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    documento_identidad BIGINT,
+    fecha_nacimiento DATE,
+    direccion VARCHAR(255),
+    telefono VARCHAR(50),
+    id_rol INT,
+    salario_base DECIMAL(10, 0) NOT NULL ,
+
+    CONSTRAINT fk_usuario_rol FOREIGN KEY (id_rol)
+       REFERENCES rol(id_rol)
+       ON DELETE SET NULL
+       ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
