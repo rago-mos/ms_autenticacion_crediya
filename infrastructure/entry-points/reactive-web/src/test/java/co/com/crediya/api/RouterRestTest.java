6be57807cd1625e46009fbae1b9604b8fe6941fd
rago.mos <ralberto115@gmail.com>
2025-08-27 21:36:36 -0500

    test: ajuste de test de acuerdo a refactorizacion

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/RouterRestTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/RouterRestTest.java
@@ -1,6 +1,7 @@
 package co.com.crediya.api;
 
 import co.com.crediya.api.dto.CreateUserRequest;
+import co.com.crediya.api.dto.CreateUserResponse;
 import co.com.crediya.api.exception.GlobalExceptionHandler;
 import co.com.crediya.api.mapper.UserMapper;
 import co.com.crediya.model.role.Role;
@@ -20,6 +21,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static reactor.blockhound.shaded.net.bytebuddy.matcher.ElementMatchers.any;
 
@@ -43,10 +45,22 @@ class RouterRestTest {
     void shouldRegisterSuccessfully() {
         User user = userMock();
         CreateUserRequest request = userRequestMock();
+        CreateUserResponse response = CreateUserResponse.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .identityDocument(user.getIdentityDocument())
+                .birthDate(user.getBirthDate())
+                .address(user.getAddress())
+                .phoneNumber(user.getPhoneNumber())
+                .rol(user.getRole().getName())
+                .baseSalary(user.getBaseSalary())
+                .build();
 
         when(validator.validate(any())).thenReturn(Set.of());
         when(userMapper.toModel(request)).thenReturn(user);
         when(userUseCase.execute(user)).thenReturn(Mono.just(user));
+        when(userMapper.toResponse(user)).thenReturn(response);
 
         webTestClient.post()
                 .uri("/api/v1/usuarios")
@@ -55,7 +69,13 @@ class RouterRestTest {
                 .bodyValue(request)
                 .exchange()
                 .expectStatus().isCreated()
-                .expectBody(String.class).isEqualTo("User created successfully");
+                .expectBody(CreateUserResponse.class)
+                .value(r -> {
+                    assertEquals("Pepe", r.firstName());
+                    assertEquals("Perez", r.lastName());
+                    assertEquals("pepe@gmail.com", r.email());
+                    assertEquals("CLIENTE", r.rol());
+                });
     }
 
 
