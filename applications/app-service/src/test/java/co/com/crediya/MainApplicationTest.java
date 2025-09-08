227cf8a3df18d3bbbb30029c63a5ac9e7b2092c1
rago.mos <ralberto115@gmail.com>
2025-09-08 18:13:22 -0500

    feat: ajustes relacionados hu06

--- applications/app-service/src/test/java/co/com/crediya/MainApplicationTest.java
+++ applications/app-service/src/test/java/co/com/crediya/MainApplicationTest.java
@@ -1,26 +1,31 @@
 package co.com.crediya;
 
+import co.com.crediya.r2dbc.RoleReactiveRepository;
+import co.com.crediya.r2dbc.UserReactiveRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(
         classes = MainApplication.class,
         properties = {
                 "jwt.secret=my-super-secret-key-12345678901234567890123456789012",
-                "jwt.expiration=3600000"
+                "jwt.expiration=3600000",
+                "server.port=8002",
+                "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration",
+                "spring.flyway.enabled=false",
         }
 )
 class MainApplicationTest {
+
+    @MockitoBean
+    private RoleReactiveRepository roleReactiveRepository;
+
+    @MockitoBean
+    private UserReactiveRepository userReactiveRepository;
 
     @Test
     void contextLoads() {
     }
 
-    @Test
-    void shouldRunMainWithoutErrors() {
-        System.setProperty("jwt.secret", "my-super-secret-key-12345678901234567890123456789012");
-        System.setProperty("jwt.expiration", "3600000");
-
-        MainApplication.main(new String[]{});
-    }
 }
