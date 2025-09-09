36cfcee217bb93e8e518d540ea56820c6c23313c
rago.mos <ralberto115@gmail.com>
2025-09-08 22:35:12 -0500

    doc: ajuste a documentación swagger dividida en handler y routerRest

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/Handler.java
@@ -1,13 +1,20 @@
 package co.com.crediya.api;
 
-import co.com.crediya.api.dto.CreateUserRequest;
-import co.com.crediya.api.dto.LoginRequest;
-import co.com.crediya.api.dto.UserApplicationsRequest;
+import co.com.crediya.api.dto.*;
 import co.com.crediya.api.mapper.LoginMapper;
 import co.com.crediya.api.mapper.UserMapper;
 import co.com.crediya.api.validator.RequestValidator;
+import co.com.crediya.model.login.TokenDTO;
 import co.com.crediya.usecase.createuser.ICreateUserUseCase;
 import co.com.crediya.usecase.createuser.ILoginUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -38,6 +45,46 @@ public class Handler {
     private final LoginMapper loginMapper;
 
 
+
+    @Operation(operationId = "createUser",
+            summary = "Register a new user",
+            description = "The system receives user data and sends a registration confirmation",
+            requestBody = @RequestBody(required = true,
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CreateUserRequest.class)
+                    )
+            ),
+            responses = {@ApiResponse(responseCode = "201",
+                    description = "User created successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CreateUserResponse.class)
+                    )
+            ), @ApiResponse(responseCode = "400",
+                    description = "Invalid request format",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseHandler.class)
+                    )
+            ), @ApiResponse(responseCode = "403",
+                    description = "Access denied",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseHandler.class)
+                    )
+            ), @ApiResponse(responseCode = "409",
+                    description = "Conflict: Email already exists or salary is invalid",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseHandler.class),
+                            examples = {
+                                    @ExampleObject(name = "EmailConflict", value = "{\"timestamp\": \"2025-08-26T14:22:56.402Z\",\"status\":409,\"error\":\"BusinessException\",\"message\":\"A user with this email already exists\"}"),
+                                    @ExampleObject(name = "SalaryConflict", value = "{\"timestamp\": \"2025-08-26T14:22:56.402Z\",\"status\":409,\"error\":\"BusinessException\",\"message\":\"The salary is not valid\"}")
+                            }
+                    )
+            ), @ApiResponse(responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseHandler.class)
+                    )
+            )}
+    )
     @PreAuthorize("hasAnyAuthority('ADMIN', 'ASESOR')")
     public Mono<ServerResponse> listenPostCreateUser(ServerRequest request) {
         return request.bodyToMono(CreateUserRequest.class)
@@ -53,6 +100,42 @@ public class Handler {
             );
     }
 
+
+    @Operation(
+            operationId = "userExistsByDocument",
+            summary = "Verify if a user exists by document number",
+            parameters = {
+                    @Parameter(
+                            name = "documentIdentity",
+                            in = ParameterIn.PATH,
+                            required = true,
+                            description = "User's document number to verify."
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Verification result",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    examples = @ExampleObject(
+                                            name = "exists",
+                                            value = "{\"exists\": true}"
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Parameter 'document' invalid or blank"
+                    ),
+                    @ApiResponse(responseCode = "403",
+                            description = "Access denied",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseHandler.class)
+                            )
+                    ),
+            }
+    )
     @PreAuthorize("hasAuthority('CLIENTE')")
     public Mono<ServerResponse> listenGetUserByDocument(ServerRequest request) {
 
@@ -67,6 +150,37 @@ public class Handler {
             });
     }
 
+
+    @Operation(operationId = "getUserApplications",
+            summary = "User query by document",
+            description = "The system receives the user's document and returns an object with basic user information",
+            requestBody = @RequestBody(required = true,
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserApplicationsRequest.class)
+                    )
+            ),
+            responses = {@ApiResponse(responseCode = "200",
+                    description = "Data returned successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserApplicationResponse.class)
+                    )
+            ), @ApiResponse(responseCode = "400",
+                    description = "Invalid request format",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseHandler.class)
+                    )
+            ), @ApiResponse(responseCode = "403",
+                    description = "Access denied",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseHandler.class)
+                    )
+            ), @ApiResponse(responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseHandler.class)
+                    )
+            )}
+    )
     @PreAuthorize("hasAuthority('ASESOR')")
     public Mono<ServerResponse> listenPostUserApplications(ServerRequest request) {
 
@@ -82,6 +196,29 @@ public class Handler {
                             .bodyValue(res));
     }
 
+
+    @Operation(
+            operationId = "tokenCreate",
+            summary = "Generate token",
+            requestBody = @RequestBody(required = true,
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = LoginRequest.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "token created successfully",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = TokenDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Bad credentials"
+                    )
+            }
+    )
     public Mono<ServerResponse> listenPostLogin(ServerRequest request) {
 
         return request.bodyToMono(LoginRequest.class)
